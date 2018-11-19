254c3e9ecd67efbc97d8fc8078e1074b06f8c4ff
Fredrick Barfield <flbarfield@yahoo.com>
2018-11-18 21:46:54 -0700

    Add files via upload

--- ~AntiTextBookPy/App.py
+++ ~AntiTextBookPy/App.py
@@ -1,4 +1,7 @@
-###### First Exercise ######
+###### First Section, Operators ######
+
+# Exercise:
+
 # first = int(input("Please input three numbers to be multiplied. \nFirst number: "));
 # second = int(input("Second Number: "));
 # third = int(input("Third Number: "));
@@ -13,21 +16,13 @@
 
 
 
-#####Second Section#####
+##### Second Section, High Level Data Structures #####
 
 ##NOTES##
 # dir returns all methods valid for the data type! Excellent for when you get stuck with a particular object type
 # SLICING: var[a:b:c]... a = starting place. b = many element it'll count, c = how elements are incremented
 
 # Excercises
-"""
-2.
-3.
-4.
-5.
-6.
-7.
-"""
 
 #1
 # Find the number of occurances of 1 in the list [1,2,1,2,1,2,3,4,1,2,3].
@@ -82,15 +77,85 @@
 #5
 # Create a random dictionary and print key : value pair in ascending order of keys. Input: a = {"IN":"India", "ES":"Español"}, output: "ES"":"Español", "IN":"India". You have to use dir to find out the necessary functions.
 
-randoDict = {'AZ' : 'Arizona', 'TX' : 'Texas', 'UT' : 'Utah', 'MS' : 'Mississippi'}
-print(randoDict.get('AZ'))
+# randoDict = {'AZ' : 'Arizona', 'TX' : 'Texas', 'UT' : 'Utah', 'MS' : 'Mississippi'}
+# print(sorted(randoDict))
 
 #6
 # The same example as above, print in descending order.
+
+# randoDict = {'AZ' : 'Arizona', 'TX' : 'Texas', 'UT' : 'Utah', 'MS' : 'Mississippi'}
+# print(sorted(randoDict, reverse=True))
 
 #7 Given twos list, find their
 # a) common elements.
 # b) elements present in list a but not in b.
 # c) elements present in list b but not in a. hint: use sets.
 
+# -----A-----
+
+"""SAME LISTS FOR A, B, C"""
+
+# aList = ['apple', 'annie', 'bill', 'bob', 'joe']
+# bList = ['Savannah', 'apple', 'billy', 'bob', 'jackson']
+# newList = []
+
+# for i in aList:
+#     if i in bList:
+#         newList.append(i)
+# print(newList)
+
+# -----B-----
+
+# for i in aList:
+#     if i not in bList:
+#         newList.append(i)
+# print(newList)
+
+# -----C-----
+# for i in bList:
+#     if i not in aList:
+#         newList.append(i)
+# print(newList)
+
 #8 Print a reverse of a list without using the reverse method.
+
+# aList = ['apple', 'annie', 'bill', 'bob', 'joe']
+#
+# print(aList[::-1])
+
+##### Third Section, Constructs #####
+
+# Exercises:
+
+# 1 - Given that a = [1,2,3,4,[5,6,7]]
+# Delete the first value.
+
+# a = [1,2,3,4,[5,6,7]]
+
+# del a[0]
+# print(a)
+
+# Delete the second value of the list present at index 4.
+
+# del a[4]
+# print(a)
+
+# Delete the variable a.
+
+# del a
+
+# Take an input value from the user and delete it (if it is present), print "value not present" if the value is not present in the list.
+
+phraseList = []
+storedVar = input('Hey, Type anything. But type Q to quit: ')
+
+while True:
+    if 'Q' in storedVar:
+        break
+    elif storedVar in phraseList:
+        print("Hey! You've already typed that! But here's your current list.\n", phraseList)
+        storedVar = input('Try another again! Or enter "Q" to quit!: ')
+    else:
+        phraseList.append(storedVar)
+        print("Here's your current list: ", phraseList)
+        storedVar = input("Try more characters!: ")

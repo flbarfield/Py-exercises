50564821b9e126e7c5424c207f6d0f9663d14e6f
Fredrick Barfield <flbarfield@yahoo.com>
2018-11-20 16:52:02 -0700

    Add files via upload

--- ~AntiTextBookPy/App.py
+++ ~AntiTextBookPy/App.py
@@ -123,9 +123,8 @@
 #
 # print(aList[::-1])
 
-##### Third Section, Constructs #####
+"""##### Third Section, Constructs #####"""
 
-#### Exercises:
 
 # 1 - Given that a = [1,2,3,4,[5,6,7]]
 # Delete the first value.
@@ -176,15 +175,212 @@
 # Delete the variable a.
 # del a
 
-# -- More Exercises --
+"""######## -- More Exercises -- #########"""
+
 # 1
 # Take the name, age and score of the user as input. Print their percentage. If the percentage is less than 60, print F, if percentage is between 60 and 70 print B and if percentage is greater than 70 print A. Marks are out of 100.
 
-score = int(input('What was your test score? : '))
+# score = int(input('What was your test score? : '))
+#
+# if score < 60:
+#     print('YOU FAIL. LOL')
+# elif score >= 60 and score < 70:
+#     print('Ya get a B. Grats.')
+# else:
+#     print('YOU GOT AN A! YAHOO!')
 
-if score < 60:
-    print('YOU FAIL. LOL')
-elif score >= 60 and score < 70:
-    print('Ya get a B. Grats.')
-else:
-    print('YOU GOT AN A! YAHOO!')
+#2 Take a number from the user and print if it is even or odd.
+
+# storedNum = int(input('Input a number : '))
+#
+# if storedNum % 2 != 0:
+#   print('Your number is odd')
+# else:
+#   print('Your number is even')
+
+#3 Take two numbers from the user and print which number is largest.
+
+# a = int(input('Please type a number : '))
+# b = int(input('Please type another number : '))
+#
+# if a > b:
+#     print(a, ' is the larger number.')
+# elif a < b:
+#     print(b, ' is the larger number.')
+# elif a == b:
+#     print('Both of your numbers are equal.')
+
+#4 Take two numbers from the user and print which number is smallest.
+
+# a = int(input('Please type a number : '))
+# b = int(input('Please type another number : '))
+#
+# if a < b:
+#     print(a, ' is the smaller number.')
+# elif a > b:
+#     print(b, ' is the smaller number.')
+# elif a == b:
+#     print('Both of your numbers are equal.')
+
+#5 Take three numbers from the user and print largest and smallest number.
+
+# numList = list(input('Please type a number sequence, no spaces'))
+#
+# print(numList)
+#
+# numList.sort()
+#
+# print(numList[::-1])
+
+#6 Take four numbers from the user and create a list.
+
+# numList = list(input('Please type a number sequence, no spaces'))
+#
+# print('Your number list is', numList)
+
+
+#7 Take four numbers from the user and create a set.
+
+# fillSet = set()
+#
+# numList = list(input('Please type a number sequence, no spaces'))
+#
+# print('Your number list is', numList)
+#
+#
+# for i in numList:
+#     fillSet.update(i)
+# print('Your list is now a set: ', fillSet, "\nType: ", type(fillSet))
+
+
+
+""" ####### Loop Exercises: ##########"""
+
+#1
+# Print all numbers till 100.
+
+# val = [101]
+# count = 0
+#
+# while count < 101:
+#     if count != val:
+#         print(count)
+#         count += 1
+#     else:
+#         break
+
+#2 Print all numbers from 20 to 100.
+
+# val = [100]
+# count = 0
+#
+# while count < 100:
+#     if count != val:
+#         count += 1
+#         if count > 19:
+#             print(count)
+#     else:
+#         break
+
+#3 Print all even numbers less than 100.
+
+# val = [100]
+# count = 0
+#
+# while count < 100:
+#     if count != val:
+#         count += 1
+#         if count % 2 == 0:
+#             print(count)
+#     else:
+#         break
+
+#4 Print all odd numbers less than 100.
+
+# val = [100]
+# count = 0
+#
+# while count < 100:
+#     if count != val:
+#         count += 1
+#         if count % 2 != 0:
+#             print(count)
+#     else:
+#         break
+
+"""########## Exercises: Continue #########"""
+
+#1 Print all numbers from 0 till 10 except 4 and 5.
+
+# for i in range(11):
+#     if i == 4 or i == 5:
+#         continue
+#     else:
+#         print(i)
+
+#2 Print all numbers from 0 till 20 except those divisible by 3.
+
+# for i in range(21):
+#     if i % 3 == 0:
+#         print(i)
+#     else:
+#         continue
+
+#3 a = [1,2,3,4,5], take an integer as input from the user. Print all numbers which are less than the input number using break.
+
+# a = [1,2,3,4,5]
+#
+# storedInt = int(input('Please type a number 1-5 : '))
+# newList = []
+#
+# for i in a:
+#     if i < storedInt:
+#         newList.append(i)
+# print('Everything less than the number you typed is:\n', newList)
+
+# Alternative...
+
+# for i in a:
+#     if i < storedInt:
+#         print(i)
+
+##### Exercises: While ######
+
+#1
+# Print all numbers till 100 using while.
+#
+# i = 0
+#
+# while i < 101:
+#     print (i)
+#     i += 1
+
+#2 Print all numbers from 100 to 1 using while.
+
+# i = 100
+#
+# while i > 0:
+#     print (i)
+#     i -=1
+
+#3 Print all even numbers less than 100 using while.
+
+# i = 100
+#
+# while i > 0:
+#     if i % 2 == 0:
+#         print(i)
+#         i -= 1
+#     else:
+#         i -= 1
+#         continue
+
+#4 Print all odd numbers less than 100 using while.
+
+
+
+
+
+# For a list [1,2,3,4,5], write a program which checks if 6 is present in the list, using while.
+
+# Print all numbers from 0 to 10 except 4 and 5.
